0802b5f3f6f3343db78fb56bcdf700759d50976b
Lioserious <lihrig@student.42heilbronn.de>
2025-03-12 14:11:09 +0100

    added Struct

--- fract-ol.h
+++ fract-ol.h
@@ -6,7 +6,7 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 16:01:59 by lihrig            #+#    #+#             */
-/*   Updated: 2025/02/19 16:14:23 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/03/12 14:09:40 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <lib/MLX42/include/MLX42/MLX42.h>
+#include <math.h>
+#include "lib/MLX42/include/MLX42/MLX42.h"
+#include "lib/Libft/libft.h"
 
 #define WIDTH 512
 #define HEIGHT 512
+
+typedef struct s_fractal {
+    mlx_t       *mlx;
+    mlx_image_t *img;
+    int         width;
+    int         height;
+    double      min_re;
+    double      max_re;
+    double      min_im;
+    double      max_im;
+    int         max_iter;
+    int         fractal_type;  // 1: Mandelbrot, 2: Julia, 3: Weiteres Fraktal
+    double      julia_re;      // Julia-Set Parameter
+    double      julia_im;      // Julia-Set Parameter
+    int         color_scheme;  // Farbschema-Auswahl
+} t_fractal;
 
 #endif
