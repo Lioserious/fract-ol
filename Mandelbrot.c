9cac9a8d075c42f816ec5a32dfe48ff3e4f729e8
Lioserious <lihrig@student.42heilbronn.de>
2025-03-13 18:42:42 +0100

    added my render mandelbrot and adjusted it to julia

--- Mandelbrot.c
+++ Mandelbrot.c
@@ -6,7 +6,7 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 14:39:17 by lihrig            #+#    #+#             */
-/*   Updated: 2025/03/13 18:35:10 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/03/13 18:41:47 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	calculate_mandelbrot(t_complex c, int max_iter)
 {
-	int		i;
-	double	temp;
+	int			i;
+	double		temp;
 	t_complex	z;
 
 	z.real = 0.0;
@@ -30,18 +30,44 @@ int	calculate_mandelbrot(t_complex c, int max_iter)
 	}
 	return (i);
 }
-int calculate_julia(t_complex z, t_complex c, int max_iter)
+
+int	calculate_julia(t_complex z, t_complex c, int max_iter)
 {
-    int i;
-    double temp;
-    
-    i = 0;
-    while (i < max_iter && (z.real * z.real + z.imag * z.imag) < 4.0)
-    {
-        temp = z.real * z.real - z.imag * z.imag + c.real;
-        z.imag = 2.0 * z.real * z.imag + c.imag;
-        z.real = temp;
-        i++;
-    }
-    return (i);
+	int		i;
+	double	temp;
+
+	i = 0;
+	while (i < max_iter && (z.real * z.real + z.imag * z.imag) < 4.0)
+	{
+		temp = z.real * z.real - z.imag * z.imag + c.real;
+		z.imag = 2.0 * z.real * z.imag + c.imag;
+		z.real = temp;
+		i++;
+	}
+	return (i);
+}
+
+void	render_julia(t_fractal *fractal)
+{
+	int			x;
+	int			y;
+	t_complex	z;
+	t_complex	c;
+	int			iter;
+
+	c.real = fractal->julia_re;
+	c.imag = fractal->julia_im;
+	y = 0;
+	while (y < fractal->height)
+	{
+		x = 0;
+		while (x < fractal->width)
+		{
+			z = map_pixel_to_complex(fractal, x, y);
+			iter = calculate_julia(z, c, fractal->max_iter);
+			mlx_put_pixel(fractal->img, x, y, calculate_color(fractal, iter));
+			x++;
+		}
+		y++;
+	}
 }
